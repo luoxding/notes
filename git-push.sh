be498472f1d707a04de8010fb07df11514cbc214
luoxding <anthony_lambert@live.com>
2022-09-12 12:40:53 +0800

    增加了一些功能插件

---
+++ git-push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter your message"
+read message
+git add --all
+git commit -m "${message}"
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN"
+else
+ git status
+ echo "Pushing data to remote server!!!"
+ git push -u origin main
+fi
